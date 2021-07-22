978d8defaab2a3150ca0394d8caa92b8a4f6cbf8
Steve G <9045562+eurogig@users.noreply.github.com>
2021-07-22 16:02:36 +0100

    removed spelling error

--- yor.rb
+++ yor.rb
@@ -1,7 +1,7 @@
 class Yor < Formula
   desc "Extensible auto-tagger for your IaC files"
   homepage "https://www.bridgecrew.io"
-  version: "0.1.84"
+  version "0.1.84"
   url "https://github.com/bridgecrewio/yor/releases/download/$($version)/yor-$($version)-darwin-amd64.tar.gz"
   sha256 "5c3e44b89ced43365bb91405001fbb7eef5b48b0cea61ace68b6a44efbbb2b8e"
   license "Apache-2.0"
